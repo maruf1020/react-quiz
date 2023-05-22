2d7ab03195aa66252ce95b6a03c32edf8d379368
maruf1020 <marufbillah03033@gmail.com>
2023-05-23 02:00:14 +0600

    update code

--- firebase.js
+++ firebase.js
@@ -5,6 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+    // eslint-disable-next-line no-undef
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // eslint-disable-next-line no-undef
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // eslint-disable-next-line no-undef
+    projectId: process.env.REACT_APP_FIREBASE_PROJET_ID,
+    // eslint-disable-next-line no-undef
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // eslint-disable-next-line no-undef
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // eslint-disable-next-line no-undef
+    appId: process.env.REACT_APP_FIREBASE_API_ID
 
 };
 
