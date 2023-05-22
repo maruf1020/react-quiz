ef80c8bf2ed0b1e39d8b5e4fe6a15eea550be1d2
maruf1020 <marufbillah1020@gmail.com>
2023-05-22 17:18:40 +0600

    Update Code

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+import "../styles/App.css";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <Layout>
+      <Home></Home>
+    </Layout>
+  );
+}
+
+export default App;
