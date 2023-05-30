8b149cd1ddcee31b8f6df0fdef9c46b9d19cc804
maruf1020 <marufbillah03033@gmail.com>
2023-05-31 03:00:20 +0600

    Update code

--- src/components/MiniPlayer.jsx
+++ src/components/MiniPlayer.jsx
@@ -1,14 +1,34 @@
+import { useRef, useState } from "react";
 import image from "../assets/images/3.jpg";
 import classes from "../styles/MiniPlayer.module.css";
 
 export default function MiniPlayer() {
+  const buttonRef = useRef();
+  const [open, setOpen] = useState(false);
+
+  function toggleMiniPlayer() {
+    if (!open) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setOpen(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setOpen(false);
+    }
+  }
+
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
-      <span className={`material-icons-outlined ${classes.open}`}>
+    <div
+      className={`${classes.miniPlayer} ${classes.floatingBtn}`}
+      ref={buttonRef}>
+      <span
+        className={`material-icons-outlined ${classes.open}`}
+        onClick={toggleMiniPlayer}>
         {" "}
         play_circle_filled{" "}
       </span>
-      <span className={`material-icons-outlined ${classes.close}`}>
+      <span
+        className={`material-icons-outlined ${classes.close}`}
+        onClick={toggleMiniPlayer}>
         {" "}
         close{" "}
       </span>
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,19 +1,38 @@
+import { useRef, useState } from "react";
 import classes from "../styles/ProgressBar.module.css";
 import Button from "./Button";
 
 // eslint-disable-next-line react/prop-types
 export default function ProgressBar({ next, prev, submit, parentage }) {
+  const [tooltip, setTooltip] = useState(false);
+  const toolTipRef = useRef();
+
+  function toggleTooltip() {
+    if (tooltip) {
+      setTooltip(false);
+      toolTipRef.current.style.display = "none";
+    } else {
+      setTooltip(true);
+      toolTipRef.current.style.display = "block";
+      toolTipRef.current.style.left = `calc(${parentage}% - 65px)`;
+    }
+  }
+
   return (
     <div className={classes.progressBar}>
       <div className={classes.backButton} onClick={prev}>
         <span className="material-icons-outlined"> arrow_back </span>
       </div>
       <div className={classes.rangeArea}>
-        <div className={classes.tooltip}>{parentage}% Complete!</div>
+        <div className={classes.tooltip} ref={toolTipRef}>
+          {parentage}% Complete!
+        </div>
         <div className={classes.rangeBody}>
           <div
             className={classes.progress}
-            style={{ width: `${parentage}%` }}></div>
+            style={{ width: `${parentage}%` }}
+            onMouseOver={toggleTooltip}
+            onMouseOut={toggleTooltip}></div>
         </div>
       </div>
       <Button
